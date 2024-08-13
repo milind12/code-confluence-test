901dc6f170dc18f2cc1f767ec341197707edab50
Milind <milind.12ghiya@gmail.com>
2024-08-13 13:56:36 +0530

    fix docusaurus config

--- code-confluence/docusaurus.config.js
+++ code-confluence/docusaurus.config.js
@@ -13,14 +13,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://unoplat.github.io',
+  url: 'https://milind12.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/unoplat-code-confluence/code-confluence/production/',
+  baseUrl: '/code-confluence-test/code-confluence/production/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'unoplat', // Usually your GitHub org/user name.
+  organizationName: 'milind12', // Usually your GitHub org/user name.
   projectName: 'code-confluence', // Usually your repo name.
 
   onBrokenLinks: 'throw',
